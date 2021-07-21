bb5fe12641f7c85e8754154de0321739a2c1265d
thinkbim <thinkbim@thinkbim.cn>
2021-07-21 17:38:58 +0800

    百度统计

---
+++ baidu/js/baidu.js
@@ -0,0 +1,10 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fddad4d925413d07f9e0ee3a15fa99e0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
