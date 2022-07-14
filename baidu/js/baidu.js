fae04414f6734daba45c27e28bbced7bb3e3081e
ThinkBIM <thinkbim@thinkbim.cn>
2022-07-14 10:43:52 +0800

    Update baidu.js

--- baidu/js/baidu.js
+++ baidu/js/baidu.js
@@ -1,8 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?fddad4d925413d07f9e0ee3a15fa99e0";
+  hm.src = "https://hm.baidu.com/hm.js?9a6499eacecd7b2a408fc0aabdf3eab3";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
