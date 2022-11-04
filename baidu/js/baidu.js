e86f7040816759e5f9e3736b443e7b3d3b25325d
thinkbim <thinkbim@thinkbim.cn>
2022-11-04 11:42:28 +0800

    post chrome.md

--- baidu/js/baidu.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?9a6499eacecd7b2a408fc0aabdf3eab3";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-
